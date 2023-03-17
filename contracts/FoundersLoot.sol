f4f458f4cdd70bf173abc8785b7032f6ef3289f1
Semi Invader <=>
2023-03-17 14:47:42 -0600

    switch to much simpler implementation

--- contracts/FoundersLoot.sol
+++ contracts/FoundersLoot.sol
@@ -5,147 +5,113 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./BunnyAiToken.sol";
 
+interface IBunaiNft is IERC721 {
+    function totalSupply() external returns (uint);
+}
+
 contract FoundersLoot is Ownable {
-    struct User {
+    struct NFTClaim {
         uint256 accTracker; // Track the rewards accumulated at any point to reduce already claimed amounts
         uint256 totalClaimed; // Track the rewards claimed by user in ETH
-        uint8 stakedNFTs;
     }
-    mapping(uint => address) public idStakedBy; // used to recover NFTs on withdraw
-    mapping(address => User) public users;
+    mapping(uint8 => NFTClaim) public claimed;
+
     uint256 public constant MAGNIFY_FACTOR = 1e12;
     // NFT address
-    IERC721 public founderNFT;
+    IBunaiNft public founderNFT;
     // BUNAI address
     BunnyAiToken public bunai;
-    address public marketingWallet;
-    uint256 accumulatedRewardsPerNFT;
+    uint256 public accumulatedRewardsPerNFT;
     uint256 public burnBunai;
-    uint8 public totalStakedNFTs;
+    bool public reentrancyGuard;
+    uint256 public totalNFTs;
 
-    event Deposit(address indexed user, uint256 indexed nftId);
-    event Withdraw(address indexed user, uint256 indexed nftId);
+    modifier nonReentrant() {
+        require(!reentrancyGuard, "ReentrancyGuard: reentrant call");
+        reentrancyGuard = true;
+        _;
+        reentrancyGuard = false;
+    }
     event Claim(address indexed user, uint256 amount);
-    event Forfeit(address indexed user, uint256 amount);
 
     constructor(address _founderNFT, address payable _bunai) {
         require(
             _founderNFT != address(0) && _bunai != address(0),
             "Invalid addresses"
         );
-        founderNFT = IERC721(_founderNFT);
+        founderNFT = IBunaiNft(_founderNFT);
         bunai = BunnyAiToken(_bunai);
+        totalNFTs = founderNFT.totalSupply();
+        burnBunai = 100 ether;
+        require(totalNFTs > 0, "Invalid address");
     }
 
     ///@notice automatically increases reward based on ETH amount received.
     ///@dev if there are not staked NFT's we send the ETH to the marketing wallet
     receive() external payable {
-        if (totalStakedNFTs > 0)
-            accumulatedRewardsPerNFT +=
-                (msg.value * MAGNIFY_FACTOR) /
-                totalStakedNFTs;
-        else {
-            (bool succ, ) = marketingWallet.call{value: msg.value}("");
-            require(succ, "ETH marketing transfer failed");
-        }
+        accumulatedRewardsPerNFT += (msg.value * MAGNIFY_FACTOR) / totalNFTs;
     }
 
-    ///@notice Deposit NFT to start receiving rewards
-    ///@param _nftId NFT ID to deposit
-    ///@dev NFT is transferred to this contract
-    function deposit(uint256 _nftId) external {
-        require(founderNFT.ownerOf(_nftId) == msg.sender, "Not NFT owner");
-        User storage user = users[msg.sender];
-        // ----------------- CLAIM ANY REWARDS FIRST -----------------
-        _claim(msg.sender, user);
-        // Transfer NFT to this contract
-        founderNFT.transferFrom(msg.sender, address(this), _nftId);
-        idStakedBy[_nftId] = msg.sender;
-        user.stakedNFTs++;
-        user.accTracker = accumulatedRewardsPerNFT * user.stakedNFTs;
-        totalStakedNFTs++;
-        emit Deposit(msg.sender, _nftId);
-    }
-
-    ///@notice Withdraw NFT and claim any pending rewards
-    ///@param _nftId NFT ID to withdraw
-    function withdrawNFT(uint _nftId) external {
-        require(idStakedBy[_nftId] == msg.sender, "Not NFT owner");
-        User storage user = users[msg.sender];
-        // ----------------- CLAIM ANY REWARDS FIRST -----------------
-        _claim(msg.sender, user);
-        // Transfer NFT to user
-        founderNFT.transferFrom(address(this), msg.sender, _nftId);
-        idStakedBy[_nftId] = address(0);
-        user.stakedNFTs--;
-        user.accTracker = accumulatedRewardsPerNFT * user.stakedNFTs;
-        totalStakedNFTs--;
-        emit Withdraw(msg.sender, _nftId);
-    }
-
-    ///@notice Withdraw NFT and forfeit any pending rewards
-    ///@param _nftId NFT ID to withdraw
-    ///@dev NFT is transferred to the user and any pending rewards are sent to marketing wallet
-    function withdrawNFTAndForfeit(uint _nftId) external {
-        require(
-            idStakedBy[_nftId] == msg.sender &&
-                founderNFT.ownerOf(_nftId) == address(this),
-            "Not NFT owner"
-        );
-        User storage user = users[msg.sender];
-        //  FORFEIT ANY PENDING REWARDS
-        uint256 pending = pendingRewards(msg.sender);
-        if (pending > 0) {
-            (bool succ, ) = payable(marketingWallet).call{value: pending}("");
-            require(succ, "ETH marketing transfer failed");
-        }
-        // Transfer NFT to user
-        founderNFT.transferFrom(address(this), msg.sender, _nftId);
-        idStakedBy[_nftId] = address(0);
-        user.stakedNFTs--;
-        user.accTracker = accumulatedRewardsPerNFT * user.stakedNFTs;
-        totalStakedNFTs--;
-        emit Withdraw(msg.sender, _nftId);
+    function sendETH(uint _amount) private {
+        require(_amount > 0, "Nothing to claim");
+        require(_amount <= address(this).balance, "Insufficient balance");
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "Error on ETH Transfer");
     }
 
     ///@notice External claim function
-    function claim() external {
-        User storage user = users[msg.sender];
-        _claim(msg.sender, user);
+    function claim(uint8 _tokenId) external nonReentrant {
+        uint reward = _claim(_tokenId);
+        if (reward != 0) _burnToClaim(1);
+        sendETH(reward);
+    }
+
+    function claimMultiple(uint8[] calldata _tokenIds) external nonReentrant {
+        uint totalReward = 0;
+        for (uint8 i = 0; i < _tokenIds.length; i++) {
+            totalReward += _claim(_tokenIds[i]);
+        }
+        if (totalReward > 0) _burnToClaim(_tokenIds.length);
+        sendETH(totalReward);
     }
 
     ///@notice Claim any pending rewards
     ///@dev To claim pending rewards a BUNAI burn needs to occur by accessing the BUNAI `burnFrom` function
-    function _claim(address account, User storage user) private {
-        _burnToClaim();
-        uint256 pending = pendingRewards(account);
-        if (pending > 0) {
-            user.totalClaimed += pending;
-            (bool succ, ) = payable(account).call{value: pending}("");
-        }
-        user.accTracker = accumulatedRewardsPerNFT * user.stakedNFTs;
-        emit Claim(account, pending);
+    function _claim(uint8 _tokenId) private returns (uint _totalReward) {
+        require(_tokenId <= totalNFTs && _tokenId != 0, "Invalid token");
+        require(founderNFT.ownerOf(_tokenId) == msg.sender, "NFT: Not owner");
+        NFTClaim storage nft = claimed[_tokenId];
+        _totalReward =
+            (accumulatedRewardsPerNFT - nft.accTracker) /
+            MAGNIFY_FACTOR;
+        nft.accTracker = accumulatedRewardsPerNFT;
+        nft.totalClaimed += _totalReward;
+        emit Claim(msg.sender, _totalReward);
     }
 
     ///@notice Calculate pending rewards for user
-    ///@param _user User address
+    ///@param _tokenId id of the NFT
     ///@return pending rewards in ETH
-    function pendingRewards(address _user) public view returns (uint256) {
-        User memory user = users[_user];
-        uint256 pending = 0;
-        if (user.stakedNFTs > 0) {
-            pending =
-                ((accumulatedRewardsPerNFT * user.stakedNFTs) /
-                    MAGNIFY_FACTOR) -
-                user.accTracker;
+    function pendingRewards(uint8 _tokenId) public view returns (uint256) {
+        if (_tokenId > totalNFTs || _tokenId == 0) return 0;
+        NFTClaim storage nft = claimed[_tokenId];
+        return (accumulatedRewardsPerNFT - nft.accTracker) / MAGNIFY_FACTOR;
+    }
+
+    function pendingRewardFromMultiple(
+        uint8[] calldata _tokenIds
+    ) external view returns (uint256 totalPending) {
+        totalPending = 0;
+        for (uint8 i = 0; i < _tokenIds.length; i++) {
+            totalPending += pendingRewards(_tokenIds[i]);
         }
-        return pending;
     }
 
     ///@notice Burn BUNAI
+    ///@param tokens amount of tokens to claim
     ///@dev burn the BUNAI amount set in the burnBunai variable
-    function _burnToClaim() internal {
-        bunai.burnFrom(msg.sender, burnBunai);
+    function _burnToClaim(uint tokens) internal {
+        if (burnBunai > 0) bunai.burnFrom(msg.sender, burnBunai * tokens);
     }
 
     ///@notice Owner can change the BUNAI amount to burn
